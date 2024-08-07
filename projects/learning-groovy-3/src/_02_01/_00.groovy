757cdf8351d87d3943e49315065babb1f345e01b
luotao <luotaoyeah@gmail.com>
2024-08-08 00:20:56 +0800

    feat(learning-groovy-3): 2.1 what is groovy

---
+++ projects/learning-groovy-3/src/_02_01/_00.groovy
@@ -0,0 +1,10 @@
+package _02_01
+
+// 2.1 what is groovy
+
+// 任何合法的 java 代码, 一定也是合法的 groovy 代码,
+public class _00 {
+    public static void main(String[] args) {
+        System.out.println("HELLO");
+    }
+}
