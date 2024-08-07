91613c8850efa9f2e425915c6b61b5a8df1d96d0
luotao <luotaoyeah@gmail.com>
2024-08-08 02:37:56 +0800

    feat(doc): 1.1.4.3 triple-single-quoted string

--- projects/doc/src/_01_01_03_02/_00.groovy
+++ projects/doc/src/_01_01_03_02/_00.groovy
@@ -4,7 +4,7 @@ package _01_01_03_02
 // https://docs.groovy-lang.org/docs/groovy-4.0.22/html/documentation/#_quoted_identifiers
 
 
-//region runtime groovydoc
+//region
 //----------------------------------------------------------------------------------------------------
 
 // 点(.)后面的标识符可以用双引号(或者单引号)括起来, 通常当标识符包含特殊字符时, 必须括起来,
