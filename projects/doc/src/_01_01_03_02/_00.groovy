5dada75d402256fe3e1e451ed42d7f9fb5235f3b
luotao <luotaoyeah@gmail.com>
2024-08-08 02:08:19 +0800

    feat(doc): 1.1.3.2 quoted identifiers

---
+++ projects/doc/src/_01_01_03_02/_00.groovy
@@ -0,0 +1,24 @@
+package _01_01_03_02
+
+// 1.1.3.2 quoted identifiers
+// https://docs.groovy-lang.org/docs/groovy-4.0.22/html/documentation/#_quoted_identifiers
+
+
+//region runtime groovydoc
+//----------------------------------------------------------------------------------------------------
+
+// 点(.)后面的标识符可以用双引号(或者单引号)括起来, 通常当标识符包含特殊字符时, 必须括起来,
+
+// 定义一个空的 map,
+def map01 = [:]
+
+map01.name = "LUOTAO"
+map01."name 01" = "LUOTAO"
+map01.'name 02' = "LUOTAO"
+
+assert map01.name == "LUOTAO"
+assert map01."name 01" == "LUOTAO"
+assert map01.'name 02' == "LUOTAO"
+
+//----------------------------------------------------------------------------------------------------
+//endregion
